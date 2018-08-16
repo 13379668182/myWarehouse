d47c20b63cef500190765cd446006995fd220e04
13379668182 <13379668182@163.com>
2018-08-16 14:14:03 +0800

    demo1

---
+++ demo1/mysql.php
@@ -0,0 +1,24 @@
+<?php
+	//1.链接数据库
+
+	$link = mysqli_connect('localhost','root','root');
+		echo '<pre>';
+	var_dump($link);
+	
+	//2 判断是否连接成功
+	
+	//3 设置字符集
+	
+	//4 选择数据库
+	
+	//5 准备sql语句
+	
+	
+	//6发送sql语句
+	
+	//7 处理结果
+	
+	//8 关闭数据库(释放资源)
+
+
+?>
