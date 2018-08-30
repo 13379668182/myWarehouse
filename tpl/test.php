45d86cd3d7c2aa2e60d00137da14aec96256cb82
13379668182 <13379668182@163.com>
2018-08-30 16:39:03 +0800

    简单的写个一个模板引擎类

---
+++ tpl/test.php
@@ -0,0 +1,11 @@
+<?php
+include 'Tpl.php';
+$tpl = new Tpl();
+$title = '你好';
+$abc = '天气不错';
+$data = ['哈哈','呵呵'];
+  $tpl ->assign('title',$title);
+  $tpl->assign('data',$data);
+  $tpl->assign('abc',$abc);
+  $tpl->display('test.html');
+?>
