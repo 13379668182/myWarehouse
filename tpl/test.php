de0199855d6f7e3c25116a80500a2ae887928b3e
13379668182 <13379668182@163.com>
2018-09-12 18:00:17 +0800

    'test一下'

--- tpl/test.php
+++ tpl/test.php
@@ -3,7 +3,7 @@ include 'Tpl.php';
 $tpl = new Tpl();
 $title = '你好';
 $abc = '天气不错';
-$data = ['哈哈','呵呵'];
+$data = ['哈哈','呵呵he'];
   $tpl ->assign('title',$title);
   $tpl->assign('data',$data);
   $tpl->assign('abc',$abc);
